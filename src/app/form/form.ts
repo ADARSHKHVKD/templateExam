ee19ab123da0be5fed68b833e9aa0dde5fcadb18
ADARSHKHVKD <adarshkhvkd@gmail.com>
2026-05-18 22:28:55 +0530

    angular first

---
+++ src/app/form/form.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form',
+  imports: [FormsModule,CommonModule],
+  templateUrl: './form.html',
+  styleUrl: './form.css',
+})
+export class Form {
+
+  user={
+    name:'',
+    email:'',
+    password:''
+  };
+
+  onSubmit(form:any){
+    console.log("object value:",this.user)
+    console.log("form value :",form.value)
+  }
+
+}
